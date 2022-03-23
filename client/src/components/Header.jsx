86e1af8b6278a043d2d3658b2827fab52c11e2ca
pji0219 <pji219@naver.com>
2022-03-23 23:57:28 +0900

    헤더 컴포넌트 구현중

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { NavLink } from 'react-router-dom';
+
+const Base = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 62px;
+  z-index: 10;
+  background-color: royalblue;
+  box-shadow: 0px 1px 0px 0px #000;
+  text-align: center;
+`;
+
+function Header() {
+  return <div></div>;
+}
+
+export default Header;
