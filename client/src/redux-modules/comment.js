dc3ce63c7b292d84fa070a5a5d9b282123434a21
pji0219 <pji219@naver.com>
2022-04-12 06:22:52 +0900

    comment 리덕스 모듈 구현 완료

--- client/src/redux-modules/comment.js
+++ client/src/redux-modules/comment.js
@@ -1,5 +1,6 @@
 import { put, takeEvery, call } from 'redux-saga/effects';
 import axios from 'axios';
+
 import { configs } from '../config';
 
 // 타입
@@ -29,9 +30,10 @@ const DELETE_COMMENT_REQUEST_SUCCESS = 'DELETE_COMMENT_REQUEST_SUCCESS ';
 const DELETE_COMMENT_REQUEST_ERROR = 'DELETE_COMMENT_REQUEST_ERROR ';
 
 // 액션 생성 함수
-// 댓글들 조회
-export const loadComments = () => ({
+// 해당 게시물의 댓글들 조회
+export const loadComments = (articleId) => ({
   type: GET_COMMENTS_REQUEST,
+  payload: articleId,
 });
 
 // 해당 유저의 댓글들 조회
@@ -51,3 +53,218 @@ export const updateComment = (comment) => ({
   type: PUT_COMMENT_REQUEST,
   payload: comment,
 });
+
+// 댓글 삭제
+export const removeComment = (id) => ({
+  type: DELETE_COMMENT_REQUEST,
+  payload: id,
+  meta: id,
+});
+
+// 사가
+// axios 설정
+const token = localStorage.getItem('token');
+
+const config = {
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`,
+  },
+};
+
+// 해당 게시물의 댓글들 조회
+const getCommentsAPI = async (articleId) => {
+  const res = await axios
+    .get(`${configs.server.url}/comment/${articleId}`, config)
+    .then((res) => res.data);
+};
+
+function* getComments(action) {
+  try {
+    const res = yield call(getCommentsAPI, action.payload);
+
+    yield put({
+      type: GET_COMMENTS_REQUEST_SUCCESS,
+      payload: res,
+    });
+  } catch (err) {
+    yield put({
+      type: GET_COMMENTS_REQUEST_ERROR,
+      payload: err.response.data.msg,
+    });
+  }
+}
+
+// 해당 유저의 댓글들 조회
+const getCommentsByusernameAPI = async (username) => {
+  const res = await axios
+    .get(`${configs.server.url}/comment/?username=${username}`, config)
+    .then((res) => res.data);
+
+  return res;
+};
+
+function* getCommentsByusername(action) {
+  try {
+    const res = yield call(getCommentsByusernameAPI, action.payload);
+
+    yield put({
+      type: GET_COMMENTS_BY_USER_REQUEST_SUCCESS,
+      payload: res,
+    });
+  } catch (err) {
+    yield put({
+      type: GET_COMMENTS_BY_USER_REQUEST_ERROR,
+      payload: err.response.data.msg,
+    });
+  }
+}
+
+// 댓글 생성
+// id는 게시글 id
+const postCommentAPI = async (comment) => {
+  const res = await axios
+    .post(`${configs.server.url}/comment/${comment.id}`, comment, config)
+    .then((res) => res.data);
+
+  return res;
+};
+
+function* postComment(action) {
+  try {
+    const res = yield call(postCommentAPI, action.payload);
+
+    yield put({
+      type: POST_COMMENT_REQUEST_SUCCESS,
+      payload: res,
+    });
+  } catch (err) {
+    yield put({
+      type: POST_COMMENT_REQUEST_ERROR,
+      payload: err.response.data.msg,
+    });
+
+    yield alert('댓글 작성에 실패 하였습니다.');
+  }
+}
+
+// 댓글 수정
+// id는 댓글 id
+const putCommentAPI = async (comment) => {
+  const res = await axios
+    .put(`${configs.server.url}/comment/${comment.id}`, comment, config)
+    .then((res) => res.data);
+
+  return res;
+};
+
+function* putComment(action) {
+  try {
+    const res = yield call(putCommentAPI, action.payload);
+
+    yield put({
+      type: PUT_COMMENT_REQUEST_SUCCESS,
+      payload: res,
+    });
+  } catch (err) {
+    yield put({
+      type: PUT_COMMENT_REQUEST_ERROR,
+      payload: err.response.data.msg,
+    });
+
+    yield alert('댓글 수정에 실패하였습니다.');
+  }
+}
+
+// 댓글 삭제
+const deleteCommentAPI = async (param) => {
+  const res = await axios
+    .delete(`${configs.server.url}/comment/${param}`, config)
+    .then((res) => res.data);
+
+  return res;
+};
+
+function* deleteComment(action) {
+  const param = action.payload;
+  const id = action.meta;
+
+  try {
+    const res = yield call(deleteCommentAPI, param);
+
+    yield put({
+      type: DELETE_COMMENT_REQUEST,
+      payload: res,
+      id,
+    });
+  } catch (err) {
+    yield put({
+      type: DELETE_COMMENT_REQUEST_ERROR,
+      payload: err.response.data.msg,
+    });
+
+    yield alert('댓글 삭제에 실패하였습니다.');
+  }
+}
+
+export function* commentSaga() {
+  yield takeEvery(GET_COMMENTS_REQUEST, getComments);
+  yield takeEvery(GET_COMMENTS_BY_USER_REQUEST, getCommentsByusername);
+  yield takeEvery(POST_COMMENT_REQUEST, postComment);
+  yield takeEvery(PUT_COMMENT_REQUEST, putComment);
+  yield takeEvery(DELETE_COMMENT_REQUEST, deleteComment);
+}
+
+const initialState = {
+  comments: [],
+  errMsg: null,
+};
+
+export default function commentReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_COMMENTS_REQUEST:
+    case GET_COMMENTS_BY_USER_REQUEST:
+    case POST_COMMENT_REQUEST:
+    case PUT_COMMENT_REQUEST:
+    case DELETE_COMMENT_REQUEST:
+      return {
+        ...state,
+        errMsg: null,
+      };
+    case GET_COMMENTS_REQUEST_SUCCESS:
+    case GET_COMMENTS_BY_USER_REQUEST_SUCCESS:
+      return {
+        ...state,
+        comments: [...action.payload],
+      };
+    case GET_COMMENTS_REQUEST_ERROR:
+    case GET_COMMENTS_BY_USER_REQUEST_ERROR:
+      return {
+        ...state,
+        comments: [],
+        errMsg: action.payload,
+      };
+    case POST_COMMENT_REQUEST_SUCCESS:
+      return {
+        ...state,
+        comments: [...state.comments, action.payload],
+        errMsg: null,
+      };
+    case PUT_COMMENT_REQUEST_SUCCESS:
+      return {
+        ...state,
+        comments: state.comments.map((comment) =>
+          comment._id === action.payload._id ? action.payload : comment
+        ),
+        errMsg: null,
+      };
+    case DELETE_COMMENT_REQUEST_SUCCESS:
+      return {
+        ...state,
+        comments: state.comments.filter((comment) => comment._id !== action.id),
+        errMsg: null,
+      };
+    default:
+      return state;
+  }
+}
--- client/src/redux-modules/index.js
+++ client/src/redux-modules/index.js
@@ -3,14 +3,16 @@ import { all } from 'redux-saga/effects';
 
 import authReducer, { authSaga } from './auth';
 import articleReducer, { articleSaga } from './article';
+import commentReducer, { commentSaga } from './comment';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   article: articleReducer,
+  comment: commentReducer,
 });
 
 export function* rootSaga() {
-  yield all([authSaga(), articleSaga()]);
+  yield all([authSaga(), articleSaga(), commentSaga()]);
 }
 
 export default rootReducer;
