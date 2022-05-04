10d49be5ad79cfb59d634584ceb63002ad84954a
pji0219 <pji219@naver.com>
2022-05-05 03:52:00 +0900

    Mypage 컴포넌트 구현중

--- client/src/components/MyPage.jsx
+++ client/src/components/MyPage.jsx
@@ -1,7 +1,96 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import { FaUserCircle } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
+
+const Base = styled.div`
+  width: 100vw;
+  height: calc(100vh - 180px);
+  margin-top: 40px;
+`;
+
+const Container = styled.ul`
+  margin: 0 auto;
+  width: 1200px;
+  height: 100%;
+  background-color: #fff;
+  border-radius: 20px;
+  box-shadow: 0px 1px 5px 0px #9e9e9e;
+`;
+
+const TabContainer = styled.div`
+  width: auto;
+  height: 60px;
+  border-bottom: 1px solid #6666ff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NicknameContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 20px;
+`;
+
+const UserCircle = styled(FaUserCircle)`
+  font-size: 32px;
+  color: #6666ff;
+`;
+
+const Nickname = styled.span`
+  font-size: 24px;
+  color: #222;
+`;
+
+const Username = styled.span`
+  font-size: 24px;
+  color: #222;
+  margin-bottom: 7px;
+`;
+
+const Tab = styled.ul`
+  background-color: beige;
+  width: 500px;
+  height: 60px;
+  list-style: none;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & > .tab {
+    background-color: brown;
+    width: 240px;
+    font-size: 24px;
+    text-align: center;
+    /* &:not(:last-of-type) {
+      margin-right: 20px;
+    } */
+  }
+`;
 
 function MyPage({ username }) {
-  return <div></div>;
+  const { nickname } = useSelector((state) => state.auth);
+
+  return (
+    <Base>
+      <Container>
+        <TabContainer>
+          <NicknameContainer>
+            <UserCircle />
+            &nbsp;
+            <Nickname>{nickname}</Nickname>
+            <Username>({username})</Username>
+          </NicknameContainer>
+          <Tab>
+            <li className="tab">내가 쓴 게시물 보기</li>
+            <li className="tab">내가 쓴 댓글 보기</li>
+          </Tab>
+        </TabContainer>
+      </Container>
+    </Base>
+  );
 }
 
 export default MyPage;
